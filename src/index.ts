5d48b8b538f255bc2a100f6a1313c9101d853e46
VichoSynaptic <vicente.gonzalez@practicante5.local>
2023-02-20 12:48:42 -0300

    test: testing the CI/CD of the repo

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import { Request as Req, Response as Res } from 'express';
 
 export async function messageHandler(req: Req, res: Res) {
   try {
-    const rawData = Buffer.from(req.body.message.data, 'base64');
-    const message = JSON.parse(rawData.toString('utf-8'));
+    const rawData = Buffer.from(req.body.message.data, 'base64').toString('utf-8');
+    const message = JSON.parse(rawData);
     console.info('called');
     console.info(message);
+    res.status(200).end();
   } catch (error) {
     console.error(error);
+    res.status(200).end();
   }
-  res.status(200).end();
 }
 console.info('Server started listening to topic...');
