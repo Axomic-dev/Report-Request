d31a4af8d3a9b793043b50c616f85259465e0736
VichoSynaptic <vicente.gonzalez@practicante5.local>
2023-02-20 12:40:29 -0300

    test: testing the CI/CD of the repo

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Request as Req, Response as Res } from 'express';
 
 export async function messageHandler(req: Req, res: Res) {
   try {
-    const rawData = Buffer.from(req.body, 'base64');
+    const rawData = Buffer.from(req.body.message.data, 'base64');
     const message = JSON.parse(rawData.toString('utf-8'));
     console.info('called');
     console.info(message);
